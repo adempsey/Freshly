12eb635fb222ea8d9e4e06a128fac960687b87a4
adempsey <amd0331@gmail.com>
2014-07-24 22:08:18 -0700

    Initial Commit

---
+++ Freshly/FreshlyAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  FreshlyAppDelegate.h
+//  Freshly
+//
+//  Created by Andrew Dempsey on 7/24/14.
+//  Copyright (c) 2014 Andrew Dempsey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FreshlyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Freshly/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Freshly
+//
+//  Created by Andrew Dempsey on 7/24/14.
+//  Copyright (c) 2014 Andrew Dempsey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FreshlyAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([FreshlyAppDelegate class]));
+	}
+}
