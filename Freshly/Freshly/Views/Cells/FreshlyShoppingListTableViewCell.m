5944e4aea5eb905d3f86983d8eaead1675994e7d
adempsey <amd0331@gmail.com>
2014-08-17 14:44:19 -0400

    added checkbox to grocery list

--- Freshly/Freshly/Views/Cells/FreshlyShoppingListTableViewCell.m
+++ Freshly/Freshly/Views/Cells/FreshlyShoppingListTableViewCell.m
@@ -13,12 +13,26 @@
 #define kItemOffset 20.0
 #define kImageViewSize 25.0
 
+@interface FreshlyShoppingListTableViewCell ()
+
+@property (nonatomic, readwrite, strong) UIButton *checkBox;
+@property (nonatomic, readwrite, assign) BOOL checked;
+
+@end
+
 @implementation FreshlyShoppingListTableViewCell
 
 - (instancetype)initWithItem:(FreshlyFoodItem *)item
 {
 	if (self = [super initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:TABLE_VIEW_CELL_SHOPPING_LIST_IDENTIFIER]) {
 		[self setItem:item];
+
+		self.checkBox = [[UIButton alloc] init];
+		[self.checkBox setBackgroundImage:[UIImage imageNamed:@"checkbox_empty"] forState:UIControlStateNormal];
+		[self.checkBox addTarget:self action:@selector(toggleItemCheck) forControlEvents:UIControlEventTouchUpInside];
+		[self addSubview:self.checkBox];
+
+		self.checked = NO;
 	}
 	return self;
 }
@@ -32,17 +46,30 @@
 - (void)layoutSubviews
 {
 	[super layoutSubviews];
-	
+
+	self.checkBox.frame = CGRectMake(kItemOffset, (self.frame.size.height - 20) / 2, 20, 20);
+
 	CGRect imageViewFrame = self.imageView.frame;
-	imageViewFrame.origin.x = kItemOffset;
+	imageViewFrame.origin.x = self.checkBox.frame.origin.x + self.checkBox.frame.size.width + kItemOffset;
 	imageViewFrame.origin.y = (self.frame.size.height - kImageViewSize) / 2;
 	imageViewFrame.size.width = kImageViewSize;
 	imageViewFrame.size.height = kImageViewSize;
 	self.imageView.frame = imageViewFrame;
-	
+
 	CGRect textLabelFrame = self.textLabel.frame;
 	textLabelFrame.origin.x = imageViewFrame.origin.x + imageViewFrame.size.width + kItemOffset;
 	self.textLabel.frame = textLabelFrame;
+}
+
+- (void)toggleItemCheck
+{
+	if (self.checked) {
+		[self.checkBox setBackgroundImage:[UIImage imageNamed:@"checkbox_empty"] forState:UIControlStateNormal];
+		self.checked = NO;
+	} else {
+		[self.checkBox setBackgroundImage:[UIImage imageNamed:@"checkbox_checked"] forState:UIControlStateNormal];
+		self.checked = YES;
+	}
 }
 
 @end
