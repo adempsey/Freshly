90c83f0e33ff687211d03166d36cce3d1d7e4a75
adempsey <amd0331@gmail.com>
2014-08-24 02:18:48 -0400

    added terrible space label to storage cells

--- Freshly/Freshly/Views/Cells/FreshlyStorageTableViewCell.m
+++ Freshly/Freshly/Views/Cells/FreshlyStorageTableViewCell.m
@@ -11,14 +11,36 @@
 #import "NSDate+FreshlyAdditions.h"
 #import "UIImage+FreshlyAdditions.h"
 
+#define kSpaceLabelWidth 10.0
+#define kSpaceLabelHeight 10.0
+
+@interface FreshlyStorageTableViewCell ()
+
+@property (nonatomic, readwrite, strong) UILabel *spaceLabel;
+
+@end
+
 @implementation FreshlyStorageTableViewCell
 
 - (instancetype)initWithItem:(FreshlyFoodItem *)item
 {
 	if (self = [super initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:TABLE_VIEW_CELL_STORAGE_IDENTIFIER]) {
+		self.spaceLabel = [[UILabel alloc] init];
+		self.spaceLabel.font = [UIFont boldSystemFontOfSize:10.0];
+		[self addSubview:self.spaceLabel];
 		[self setItem:item];
 	}
 	return self;
+}
+
+- (void)layoutSubviews
+{
+	[super layoutSubviews];
+
+	CGFloat spaceLabelXOrigin = self.imageView.frame.origin.x + self.imageView.frame.size.width;
+	CGFloat spaceLabelYOrigin = self.imageView.frame.origin.y + self.imageView.frame.size.height - kSpaceLabelHeight/2;
+
+	self.spaceLabel.frame = CGRectMake(spaceLabelXOrigin, spaceLabelYOrigin, kSpaceLabelWidth, kSpaceLabelHeight);
 }
 
 - (void)setItem:(FreshlyFoodItem *)item
@@ -26,6 +48,7 @@
 	self.textLabel.text = item.name;
 	self.detailTextLabel.text = [NSString stringWithFormat:@"Purchased %@", item.dateOfPurchase.approximateDescription];
 	self.imageView.image = [UIImage imageForCategory:item.category withSize:50];
+	self.spaceLabel.text = @[@"R", @"F", @"P"][item.space.integerValue];
 }
 
 @end
